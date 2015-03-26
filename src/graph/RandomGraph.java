86cb9dd617224f32535a35cc6739c8d96ba246d8
Julien Rouse <julien.rouse@gmail.com>
2015-03-26 23:41:25 +0100

    randomgraph

--- src/graph/RandomGraph.java
+++ src/graph/RandomGraph.java
@@ -1,13 +1,34 @@
 package graph;
 
+import java.util.Random;
+
+import exception.VertexNotFoundException;
+
 public class RandomGraph implements RandomGraphGenerator {
 
-		
+	
 	@Override
 	public Graph generateErdosRenyiGraph(int n, float p)
-			throws IllegalArgumentException {
+			throws IllegalArgumentException{
 		
-		return null;
+		Graph g = new GraphSuccessorArray(n);
+		Random rand = new Random();
+		
+		for(int i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n-1;j++)
+			{
+				if(rand.nextDouble()<=p)
+				{
+					try {
+						g.addEdge(i,j);
+					} catch (VertexNotFoundException e) {
+						System.out.println("vertex "+i+" or "+j+" not found");
+					}
+				}
+			}
+		}
+		return g;
 	}
 
 }
